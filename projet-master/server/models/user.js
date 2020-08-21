ccc614375e9b01c49886350d001b2ecc0f1780ff
Vikas <vikassin563@gmail.com>
2020-08-21 18:42:48 +0530

    added

--- projet-master/server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
-    email : String,
-    password : String
-});
-
-module.exports = mongoose.model('user',userSchema,'users');//   (name of model,schema,collection in the database)
